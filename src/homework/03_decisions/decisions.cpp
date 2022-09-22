30eef582e8aff0207ca373d896b1a59227070673
RogerFlor77 <roger.flores@g.austincc.edu>
2022-09-22 23:24:23 +0000

    Homework 3 with Test Cases

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,69 @@
 //write include statement for decisions header
-
+#include "decisions.h"
+#include <string>
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    string MIN_SCORE;
+if (grade >= 90 && grade <= 100)
+{
+    MIN_SCORE = "A";
+}
+else if (grade >= 80 && grade < 90)
+{
+    MIN_SCORE = "B";
+}
+else if (grade >= 70 && grade < 80)
+{
+    MIN_SCORE = "C";
+}
+else if (grade >= 60 && grade < 70)
+{
+    MIN_SCORE = "D";
+}
+else if (grade >= 0 && grade < 60)
+{
+    MIN_SCORE = "F";
+}
+else if (grade < 0 || grade > 100)
+{
+    MIN_SCORE = "This is not a valid score";
+}
+
+return MIN_SCORE;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+string SWITCH_MIN_SCORE;
+
+switch (grade)
+{
+    case 90 ... 100:
+        SWITCH_MIN_SCORE = "A";
+        break;
+
+    case 80 ... 89:
+        SWITCH_MIN_SCORE = "B";
+        break;
+    
+    case 70 ... 79:
+        SWITCH_MIN_SCORE = "C";
+        break;
+
+    case 60 ... 69:
+        SWITCH_MIN_SCORE = "D";
+        break;
+
+    case 0 ... 59:
+        SWITCH_MIN_SCORE = "F";
+        break;
+    
+    default:
+        SWITCH_MIN_SCORE = "Your repsonse was invalid.";
+        break;
+}
+
+    return SWITCH_MIN_SCORE; 
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,51 @@
 //write include statements
+#include "decisions.h"
+#include <iostream>
+using namespace std;
 
 int main() 
 {
+	int User_Choice;
+	cout<< "Please select one of these options: \n";
+	
+	cout << "1 - Letter Grade using If \n";
+	
+	cout << "2 - Letter Grade using Switch \n";
+	
+	cout << "3 - Exit \n";
+
+	cin >> User_Choice;
+
+if (User_Choice == 1)
+{
+	int user_grade;
+
+	cout << "Please enter the grade ";
+
+	cin >> user_grade;
+
+	cout << get_letter_grade_using_if(user_grade) << "\n";
+}
+
+else if (User_Choice == 2)
+{
+	int user_grade;
+
+	cout << "Please enter the grade ";
+
+	cin >> user_grade;
+
+	cout << get_letter_grade_using_switch(user_grade) << "\n";
+}
+
+else if (User_Choice == 3)
+{
+	cout << "You have chosen to exit this program, thank you and goodbye!";
+}	
+	
+else if (User_Choice != 1 && User_Choice != 2 && User_Choice != 3)
+{
+	cout << "This is not a valid option from the menu \n";
+}	
 	return 0;
 }
